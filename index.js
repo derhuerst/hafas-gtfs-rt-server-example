1e9a7197588f4869b27ac6c08b026fbf937a2768
Jannis R <mail@jannisr.de>
2020-09-18 19:54:10 +0200

    hafas-monitor-trips@4.0.0

--- index.js
+++ index.js
@@ -1,21 +1,29 @@
 'use strict'
 
 const createGtfsRtWriter = require('hafas-gtfs-rt-feed/writer')
-const createHafas = require('vbb-hafas')
-const createMonitorWorker = require('hafas-monitor-trips/worker')
-const createMonitor = require('hafas-monitor-trips/orchestrator')
+const createThrottledHafas = require('vbb-hafas/throttle')
+const createMonitor = require('hafas-monitor-trips')
 const differentialToFullDataset = require('gtfs-rt-differential-to-full-dataset')
 const {createServer} = require('http')
 const computeEtag = require('etag')
 const serveBuffer = require('serve-buffer')
 
 const bbox = JSON.parse(process.argv.slice[3] || process.env.BBOX || 'null')
-const concurrency = parseInt(process.env.CONCURRENCY || '8')
 
 const onError = (err) => {
 	console.error(err)
 	process.exit(1)
 }
+
+const hafas = createThrottledHafas('hafas-gtfs-rt-server-example', {
+	throttlingLimit: 25,
+	throttlingInterval: 1000,
+})
+const monitor = createMonitor(hafas, bbox, {
+	fetchTripsInterval: 60_000, // 60s
+})
+monitor.on('error', onError)
+monitor.on('hafas-error', console.error)
 
 const {
 	out: writer,
@@ -23,31 +31,13 @@ const {
 } = createGtfsRtWriter({
 	encodePbf: false, // todo
 })
-
-const hafas = createHafas('hafas-gtfs-rt-server-example')
-const worker = createMonitorWorker(hafas, bbox, (type, data, moreData) => {
-	// todo: make this customisable
-	if (type === 'trip') writeTrip(data)
-	else if (type === 'position') {
-		writePosition(data, moreData)
-	}
-}, {
-	concurrency,
-})
-worker.on('error', onError)
-
-const monitor = createMonitor(bbox, {
-	concurrency,
-	interval: 30_000, // 30s
-})
-monitor.on('error', onError)
-monitor.start()
+monitor.on('position', (loc, movement) => writePosition(loc, movement))
+monitor.on('trip', trip => writeTrip(trip))
 
 const toFull = differentialToFullDataset({
 	ttl: 5 * 60 * 1000, // 5m
 })
 writer.pipe(toFull)
-
 let dump = Buffer.alloc(0)
 let timeModified = new Date()
 let etag = computeEtag(dump)
